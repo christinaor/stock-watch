c44e9854ceca2acea8de331ca5b6f76cc79f0a27
eddie joe b, III <50112619+ejbee3@users.noreply.github.com>
2023-05-23 13:06:53 -0400

    Create Login.jsx component

---
+++ src/components/Login.jsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+
+export default function Login() {
+  const [isLoggingIn, setIsLoggingIn] = React.useState(true);
+  const [isRegistering, setIsRegistering] = React.useState(false);
+
+  const container = {
+    border: 'solid black 3px',
+  };
+  return (
+    <div>
+      <div style={container}>
+        <button>Login</button>
+        <button>Register</button>
+        {isLoggingIn && (
+          <div>
+            <div>
+              <label>
+                Username: <input type="text" />
+              </label>
+            </div>
+            <div>
+              <label>
+                Password: <input type="text" />
+              </label>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
